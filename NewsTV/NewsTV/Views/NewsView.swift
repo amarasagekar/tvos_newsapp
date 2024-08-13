2ead05b9b7b6e51d033f671daaef391c476cf4df
“amarasagekar” <amarasagekar@gmail.com>
2024-08-13 10:05:34 +0530

    added view detail

--- NewsTV/NewsTV/Views/NewsView.swift
+++ NewsTV/NewsTV/Views/NewsView.swift
@@ -13,8 +13,34 @@ struct NewsView: View {
     var body: some View {
         ScrollView {
             LazyVStack(alignment: .leading, spacing: 48){
-                
+                ForEach(articleCategoriesVM.categoryArticles, id: \.category){
+                    ArticleCarouselView(title: $0.category.text, articles: $0.articles)
+                }
             }
+        }
+        .task(refreshTask)
+    }
+    
+    @ViewBuilder
+    private var overlayView:some View {
+        switch articleCategoriesVM.phase {
+        case .empty:
+            ProgressView()
+        case .success(let articles) where articles.isEmpty:
+            EmptyPlaceholderView(text: "No Articles", image: nil)
+            
+        case .failure(let error):
+            RetryView(text: error.localizedDescription, rettryAction: refreshTask)
+            
+        default: EmptyView()
+            
+        }
+    }
+    
+    @Sendable
+    private func refreshTask(){
+        Task{
+            await articleCategoriesVM.loadCategoryArticles()
         }
     }
 }
